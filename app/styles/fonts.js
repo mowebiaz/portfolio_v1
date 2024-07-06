f177b34747b0815010212e4a260faa40d2e3e189
mowebiaz <mowebiaz@gmail.com>
2024-07-06 12:37:02 +0200

    Add folder structure

---
+++ app/styles/fonts.js
@@ -0,0 +1,5 @@
+import { Inter } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+/*https://nextjs.org/docs/app/building-your-application/optimizing/fonts#using-multiple-fonts*/
